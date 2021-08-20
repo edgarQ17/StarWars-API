afe067e65abe1c43e50dbb7c264a5ef6a8113037
Edgar <omarquintero483@gmail.com>
2021-08-19 23:28:58 -0400

    .

--- request.js
+++ request.js
@@ -11,36 +11,40 @@ const response = await fetch(url);
 // Storing data in form of JSON
 var data = await response.json();
 console.log(data);
+console.log(data.birth_year)
+
 if (response) {
-//   hideloader();
+  hideloader();
+
 }
 show(data);
 }
 // Calling that async function
 getapi(api_url);
 // Function to hide the loader
-// function hideloader() {
-// document.getElementById('loading').style.display = 'none';
-// }
-// // Function to define innerHTML for HTML table
-// function show(data) {
-// let tab = 
-//   `<tr>
-//     <th>Name</th>
-//     <th>Office</th>
-//     <th>Position</th>
-//     <th>Salary</th>
-//    </tr>`;
+function hideloader() {
+document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+let tab = 
+  `<tr>
+    <th>Name</th>
+    <th>Height</th>
+    <th>Mass</th>
+    <th>Hair Color</th>
+    <th>Skin Color</th>
+   </tr>`;
 
-// // Loop to access all rows 
-// for (let r of data.list) {
-//   tab += `<tr> 
-// <td>${r.name} </td>
-// <td>${r.office}</td>
-// <td>${r.position}</td> 
-// <td>${r.salary}</td>          
-// </tr>`;
-// }
-// // Setting innerHTML as tab variable
-// document.getElementById("employees").innerHTML = tab;
-// }
+// Loop to access all rows 
+for (let r of data.list) {
+  tab += `<tr> 
+<td>${r.name} </td>
+<td>${r.office}</td>
+<td>${r.position}</td> 
+<td>${r.salary}</td>          
+</tr>`;
+}
+// Setting innerHTML as tab variable
+document.getElementById("employees").innerHTML = tab;
+}
